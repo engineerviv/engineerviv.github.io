ea62b939d3b60784320dbcb6da613268469fe993
engineerviv <vivianjacobvarghesemyntra@gmail.com>
2024-09-13 05:22:05 +0530

    Updated portfolio with new layout and images

--- assets/js/script.js
+++ assets/js/script.js
@@ -71,7 +71,7 @@ document.addEventListener('visibilitychange',
 
 // <!-- typed js effect starts -->
 var typed = new Typed(".typing-text", {
-    strings: ["frontend development", "backend development", "web designing", "android development", "web development"],
+    strings: ["Machine Learning", "Continuous Integration/Continuous Deployment", "Data Visualization", "Cloud Infrastructure", "Artificial Intelligence"],
     loop: true,
     typeSpeed: 50,
     backSpeed: 25,
@@ -160,16 +160,6 @@ VanillaTilt.init(document.querySelectorAll(".tilt"), {
 });
 // <!-- tilt js effect ends -->
 
-
-// pre loader start
-// function loader() {
-//     document.querySelector('.loader-container').classList.add('fade-out');
-// }
-// function fadeOut() {
-//     setInterval(loader, 500);
-// }
-// window.onload = fadeOut;
-// pre loader end
 
 // disable developer mode
 document.onkeydown = function (e) {
